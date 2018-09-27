434c1a4cbd2138b5e4439dee9b4d1cfe6ac4210f
Neha Kumar <neha@periscopedata.com>
2018-09-27 09:05:49 -0700

    added select and committing to master

--- snippets/First Test/First Test.sql
+++ snippets/First Test/First Test.sql
@@ -1,1 +1,2 @@
-'This is a test, writing in Periscope and hoping this shows up in Git'
+select 'This is a test, writing in Periscope and hoping this shows up in Git'
+--I added a select above and this comment from my computer, hoping this shows in Periscope!
