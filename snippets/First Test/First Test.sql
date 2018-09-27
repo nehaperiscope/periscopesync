5a62f817b1e71371a256be98ae5be900e4e4cb4f
Periscope Data <periscope@periscopedata.com>
2018-09-27 22:37:55 +0000

    Initial bulk import generated by Periscope

--- snippets/First Test/First Test.sql
+++ snippets/First Test/First Test.sql
@@ -1,2 +1,1 @@
-select 'This is a test, writing in Periscope and hoping this shows up in Git'
---I added a select above and this comment from my computer, hoping this shows in Periscope!
+'This is a test, writing in Periscope and hoping this shows up in Git'
